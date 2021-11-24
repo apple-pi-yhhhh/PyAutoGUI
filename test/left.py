bd2d67f6a92b3b37b6692d5e3e4b2d69e807607d
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-11-24 13:06:25 +0900

    Create left.py

---
+++ test/left.py
@@ -0,0 +1,11 @@
+import pyautogui as gui
+import time
+
+repetitions = 10 #繰り返し回数
+sleep_time = 4000 #間隔
+
+for num in range(repetitions): #回数分だけループ
+    print(num)
+    gui.rightClick(x,y)  #右クリック
+    
+    time.sleep(sleep_time/1000) #間隔
